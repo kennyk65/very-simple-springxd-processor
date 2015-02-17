6e46632d5de9f4165c827a12b908c1d002e70258
Ken Krueger <kkrueger@pivotal.io>
2015-02-16 20:27:43 -0500

    new

---
+++ src/main/java/demo/Translator.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+
+@MessageEndpoint
+public class Translator {
+
+	@Value ("$language") String language;	//	Requested language to translate into.
+
+	/**
+	 *	Translate into the given language / dialect. 
+	 */
+	@ServiceActivator(inputChannel="input", outputChannel="output")
+	public String translate(String something){
+		
+		if ("mexican".equalsIgnoreCase(language)) {
+			return something + ", no?";
+		} else if ("american-teenager".equalsIgnoreCase(language)) {
+			return "Like, " + something;
+		} 
+		
+		//	Canadian:
+		return something + ", eh?";
+	}
+}
